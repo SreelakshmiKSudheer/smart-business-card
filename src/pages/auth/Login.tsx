fbe228c3951144c426e63e81a2db2be746ee2d3d
Sreelakshmi K <sreelakshmikavenge3i3@gmail.com>
2026-07-06 15:32:02 +0530

    change to tailwind

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
-
 import logoImg from '../../assets/images/logoImg.png'
 import LoginForm from '../../components/organisms/forms/LoginForm'
 
-const Login: React.FC = () => {
+const Login = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-(--bg)">
+
+    <div className="flex flex-col items-center justify-center h-full p-10 bg-(--bg)">
       {/* Heading and Paragraph Section */}
-      <div style={{ marginBottom: '24px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <h1 style={{ fontSize: '1.875rem', fontWeight: '700', marginBottom: '8px', display: 'flex', alignItems: 'center', gap: '8px' }}>
-          <img src={logoImg} alt="SBC Logo" style={{ width: '32px', height: '32px', objectFit: 'contain' }} />
+      <div className="flex flex-col items-center justify-center text-center">
+        <h1 className="text-3xl text-(--text) font-bold mb-2 flex items-center gap-2"
+        >
+          <img src={logoImg} alt="SBC Logo" className="w-8 h-8 object-contain" />
           <span><strong>SBC</strong></span>
         </h1>
-        <p style={{ color: '#4b5563' }}>Manage enterprise smart card entities</p>
+        <p className="text-(--text-light)">Manage enterprise smart card entities</p>
       </div>
-
-
        <LoginForm />
       </div>
   )
 }
+
 export default Login
