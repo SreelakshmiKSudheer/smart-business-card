4f70d17cb2be801586c7325eaf5d721c34412b83
Sreelakshmi K <sreelakshmikavenge3i3@gmail.com>
2026-07-07 13:28:07 +0530

    admin header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,61 +1,17 @@
-import sbcLogo from "../../assets/images/sbc-logo.png";
+import { User } from "lucide-react";
+import { useNavigate } from "react-router";
+import Button from "../atoms/buttons";
 
-interface HeaderProps {
-  companyLogo: string;
-  companyName: string;
-}
+export default function Header() {
+  const navigate = useNavigate();
 
-export default function Header({
-  companyLogo,
-  companyName,
-}: HeaderProps) {
   return (
-    <header
-      className="
-        sticky
-        top-0
-        z-50
-        bg-white
-        border-b
-        border-slate-200
-        shadow-sm
-      "
-    >
-      <div
-        className="
-          mx-auto
-          flex
-          h-20
-          max-w-7xl
-          items-center
-          justify-between
-          px-6
-        "
-      >
-        {/* Client Company */}
-
-        <img
-          src={companyLogo}
-          alt={companyName}
-          className="h-10 object-contain"
-        />
-
-        {/* SBC */}
-
-        <div className="flex items-center gap-2">
-
-          <span className="hidden text-sm text-slate-500 md:block">
-            Powered by
-          </span>
-
-          <img
-            src={sbcLogo}
-            alt="SBC"
-            className="h-8 object-contain"
-          />
-
-        </div>
-      </div>
+    <header className="sticky top-0 z-50 w-full h-[8%] bg-(--card) border-b border-slate-200 shadow-sm flex justify-end">
+      <Button
+        icon={<User size={22} />}
+        variant="none"
+        onClick={() => navigate("/admin/profile")}
+      />
     </header>
   );
 }
