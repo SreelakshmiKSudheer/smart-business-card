0e0cd730e67abd78e035e36b84bb0dbbb49d81cb
Sreelakshmi K <sreelakshmikavenge3i3@gmail.com>
2026-07-07 12:36:37 +0530

    add header

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,14 +1,21 @@
 import { Outlet } from "react-router";
 import Sidebar from "../components/organisms/sidebar/Sidebar";
+import Header from "../components/organisms/Header";
+import Footer from "../components/organisms/Footer";
+import { company } from '../data/company'
 
 const AdminLayout = () => {
   return (
     <div className="flex h-screen">
 
       <Sidebar />
-      <main className="flex-1 overflow-auto p-6 bg-(--bg)">
-        <Outlet />
-      </main>
+      <div className="flex flex-col">
+        <Header />
+        <main className="flex-1 overflow-auto p-6 bg-(--bg)">
+          <Outlet />
+        </main>
+        <Footer company={company} />
+      </div>
     </div>
   );
 };
