38a8545e8301b83a59f4d0cc5c4d9e15109d03e7
Sreelakshmi K <sreelakshmikavenge3i3@gmail.com>
2026-07-07 10:31:07 +0530

    chart

---
+++ src/data/scanAnalytics.ts
@@ -0,0 +1,15 @@
+export type WeekData = {
+    day: string;
+    value: number;
+  };
+
+export const weekData: WeekData[] = [
+    { day: 'Monday', value: 10 },
+    { day: 'Tuesday', value: 15 },
+    { day: 'Wednesday', value: 12 },
+    { day: 'Thursday', value: 23 },
+    { day: 'Friday', value: 18 },
+    { day: 'Saturday', value: 17 },
+    { day: 'Sunday', value: 8 },
+]
+
