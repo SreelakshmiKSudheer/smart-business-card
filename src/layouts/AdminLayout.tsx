cb6a05a3d709cf2aa1d438f1d340e4a7bcf2f970
Sreelakshmi K <sreelakshmikavenge3i3@gmail.com>
2026-07-07 10:51:41 +0530

    admin layout

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -5,6 +5,7 @@ const AdminLayout = () => {
   return (
     <div className="flex h-screen">
 
+      <Sidebar />
       <main className="flex-1 overflow-auto p-6 bg-(--bg)">
         <Outlet />
       </main>
